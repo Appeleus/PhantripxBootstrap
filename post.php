98f2979fd2e9e51e18feace6b121571b0451c8c0
Appeleus <gotkunglovely@outlook.co.th>
2022-08-31 11:11:21 +0700

    The ultimate website but still no Database

---
+++ post.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhanTripDotCom</title>
+
+    <style type = "text/css">
+        #MainTable {
+            border:2px solid black;
+            width: 40%;
+        }
+
+		.MessageBox {
+            width: 100%;
+        }
+    </style>
+
+</head>
+<body>
+<h1 align = "center">PhanTrip Webboard</h1>
+    <hr> 
+    <center><b>Want to see topic discussion <?php echo $_GET["id"] ?>?</b></center>
+    <table id = "MainTable" align = "Center">
+
+        <tr><td style = "background-color:#6CD2FE;" colspan = "2">Share your opinion!</td></tr>
+		<tr><td><textarea class = "MessageBox" name = "MainBox" rows = "5" cols = "5"></textarea></td></tr>
+	<form action = "registerPage.html" method = "post">
+        <td align = "center" colspan = "2"><input type = "submit" value="Send"></td>
+    </form>
+
+    </table>
+</style>
+<p align = "Center"><a href = "WebUpgradeMain.html">Back to Main page</a></p>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 align = "center">PhanTrip Webboard</h1>
+    <hr>
+    <div align = "center">
+        Login with<br>
+        Login = <?php echo $_POST["loginMain"]; ?><br>
+        Password = <?php echo $_POST["passwordMain"]; ?>
+</body>
+</html>
